80e136aba765c66af903bcbe8bbc8683a1b40862
Andrew Balise <ajb361@miami.edu>
2020-05-18 15:19:13 -0400

    Added facts (and a lie) about Andrew.

---
+++ R/aboutAndrew.R
@@ -0,0 +1,9 @@
+#' @export aboutAndrew
+aboutAndrew <- function() {
+  cat("Which of these is a lie: \n")
+  cat("1. Andrew's grandfather was the sole survivor of an airplane crash.\n")
+  cat("2. Andrew's sister has a stuffed penguin who wants to take over the world.\n")
+  cat("3. Andrew has played the national anthem at multiple professional league sporting events.\n")
+  cat("4. Andrew is both left handed and colourblind.\n")
+  cat("5. Andrew's father was a developer of cult classic text game Zork.\n")
+}
