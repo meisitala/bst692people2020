d6a7ae56c0c01e1d9e6544ce01ddb08592ecc9b9
Christina Guzman <cig33@miami.edu>
2020-05-21 20:23:29 -0400

    Information about Christina Guzman.

---
+++ R/aboutChristina.R
@@ -0,0 +1,9 @@
+#' @export aboutChristina
+aboutChristina <- function() {
+  cat("Which of these is a lie: \n")
+  cat("1. Christina has been a certified dance instructor for the past 3 years.\n")
+  cat("2. Christina's boyfriend has read over 2000 single issues of comics.\n")
+  cat("3. Christina's grandma won the lottery in 2012.\n")
+  cat("4. Christina's sister knows the lines to every song on the In The Heights musical soundtrack.\n")
+  cat("5. Christina's mom drinks atleast 4 cups of cuban coffee a day.\n")
+}
