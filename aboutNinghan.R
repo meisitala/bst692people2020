f225484c57e2ad6c1b29522ddb193a0b57ee6efa
Ninghan Duan <nxd485@miami.edu>
2020-05-22 11:07:19 -0400

    Added facts (and a lie) about Ninghan.

---
+++ aboutNinghan.R
@@ -0,0 +1,9 @@
+#' @export aboutNinghan
+aboutNinghan <- function() {
+  cat("Which of these is a lie: \n")
+  cat("1. Ninghan's hometown is Beijing.\n")
+  cat("2. Ninghan likes playing Go.\n")
+  cat("3. Ninghan's mother is a teacher.\n")
+  cat("4. Ninghan has stayed at home for two months.\n")
+  cat("5. Ninghan Went to Australia last year.\n")
+}
