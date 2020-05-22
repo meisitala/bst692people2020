cace854127f793c242fcb55d7c281f4ef14e7d7f
Hello <jxl686@miami.edu>
2020-05-22 09:37:39 -0400

    Added facts (and a lie) about Jiangnan.

---
+++ R/aboutJiangnan.R
@@ -0,0 +1,14 @@
+#' The aboutJiangnan Function
+#'
+#' @description This is four truths and one lie.... The lie is the fifth.
+#'
+#' @return NA
+#' @export aboutJiangnan
+aboutJiangnan <- function() {
+  cat("Which of these is a lie: \n")
+  cat("1. Jiangnan's father hates watching Jiangnan doing howework.\n")
+  cat("2. Jiangnan's mother hates watching Jiangnan doing howework.\n")
+  cat("3. Jiangnan's brother hates watching Jiangnan doing howework.\n")
+  cat("4. Jiangnan's loves doing howework.\n")
+  cat("5. Jiangnan never lies.\n")
+}
